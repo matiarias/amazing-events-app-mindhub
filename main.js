fed2be912bc454c5d82605ccfa3e7adeed9b0664
Matias Arias <matt.arias182@gmail.com>
2023-04-17 18:32:16 -0300

    se agrego funcion para filtrar con un input y funcion para agregar input checkbox

--- main.js
+++ main.js
@@ -1,34 +1,80 @@
 let containerGridHome = document.querySelector(".container-grid-home");
 
-let checkboxs = document.querySelectorAll("input[type=checkbox]");
+let categoriesChecks = document.querySelector("#categories-checks");
+
+let checkboxs = document.querySelectorAll("input[type=checkbox]:checked");
 
 let inputFilter = document.querySelector(".input-filter");
 
-console.log(inputFilter);
+// ---------------------------- fetch data ----------------------------------
 
-console.log(checkboxs);
+let dataGlob = {}; // objeto global donde se guarda toda la data de la api
 
 const dataUrl = "https://mindhub-xj03.onrender.com/api/amazing";
 
-fetch(dataUrl)
-  .then((response) => response.json())
-  .then((results) => {
-    cardsHome(results);
-    // console.log(results);
-  })
-  .catch((error) => console.log(error));
+const fetchData = async () => {
+  await fetch(dataUrl)
+    .then((response) => response.json())
+    .then((results) => {
+      dataGlob = results;
+      cardsHome("");
 
-// -------------------------- Cards ---------------------------------
+      let categories = extractCategories(dataGlob.events);
+      checks(categories);
+    })
+    .catch((error) => console.log(error));
+};
 
-const cardsHome = (data) => {
-  const events = data.events;
-  // const currentDate = data.currentDate;
+// ------------------------- checkbox function -----------------------
 
-  // console.log(currentDate);
+const extractCategories = (events) => {
+  let categories = Array.from(new Set(events.map((item) => item.category)));
+
+  return categories;
+};
+
+const checks = (categories) => {
+  for (let i = 0; i < categories.length; i++) {
+    let formCheck = document.createElement("div");
+    formCheck.className = "form-check";
+    formCheck.innerHTML = `<input
+        class="checkbox-category form-check-input"
+        type="checkbox"
+        value=""
+        id="checkbox-entertainment"
+        />
+        <label class="form-check-label" for="checkbox-entertainment">
+        ${categories[i]}
+        </label>`;
+    categoriesChecks.appendChild(formCheck);
+  }
+};
+
+// -------------------------------- filter function ----------------------------
+
+inputFilter.addEventListener("keyup", () => {
+  cardsHome(inputFilter.value.toLowerCase());
+});
+
+const cardsHome = (filtro) => {
+  let events = [];
+
+  if (filtro.length === 0) {
+    events = dataGlob.events;
+  } else {
+    events = dataGlob.events.filter((item) =>
+      item.name.toLowerCase().includes(filtro)
+    );
+    console.log(events);
+  }
 
   // -------------------------------- Cards Home ------------------------------
+
+  containerGridHome.innerHTML = "";
+
   for (let i = 0; i < events.length; i++) {
     let cardContainer = document.createElement("div");
+
     cardContainer.className = "col";
     let card = document.createElement("div");
     card.className = "card w-100";
@@ -44,10 +90,10 @@ const cardsHome = (data) => {
     <span class="badge rounded-pill text-bg-secondary p-2">${events[i].date}</span>
     </div>
     <p class="card-text text-center">
-      ${events[i].description}
+    ${events[i].description}
     </p>
     <div class="d-flex justify-content-between align-items-center">
-      <span class="text-dark fw-bold">Price: ${events[i].price}</span>
+    <span class="text-dark fw-bold">Price: ${events[i].price}</span>
       <a href="#" class="btn btn-success">Details</a>
     </div>`;
 
@@ -57,3 +103,5 @@ const cardsHome = (data) => {
 };
 
 // --------------------------------------------------------------------------
+
+fetchData();
