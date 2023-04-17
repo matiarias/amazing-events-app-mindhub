8f934f2f18f47cd511f633c6f5d6580a9d957528
Matias Arias <matt.arias182@gmail.com>
2023-04-17 10:31:57 -0300

    se crearon archivos de js para todas las paginas de cards

--- main.js
+++ main.js
@@ -1,31 +1,38 @@
-let containerGrid = document.querySelector(".container-grid");
+let containerGridHome = document.querySelector(".container-grid-home");
+
+let checkboxs = document.querySelectorAll("input[type=checkbox]");
+
+let inputFilter = document.querySelector(".input-filter");
+
+console.log(inputFilter);
+
+console.log(checkboxs);
 
 const dataUrl = "https://mindhub-xj03.onrender.com/api/amazing";
 
-const data = fetch(dataUrl)
+fetch(dataUrl)
   .then((response) => response.json())
   .then((results) => {
     cardsHome(results);
-    console.log(results);
+    // console.log(results);
   })
   .catch((error) => console.log(error));
 
 // -------------------------- Cards ---------------------------------
 
-const cardsHome = (array) => {
-  const events = array.events;
-  const currentDate = array.currentDate;
+const cardsHome = (data) => {
+  const events = data.events;
+  // const currentDate = data.currentDate;
 
   // console.log(currentDate);
 
   // -------------------------------- Cards Home ------------------------------
-  if (document.URL.includes("index.html")) {
-    for (let i = 0; i < events.length; i++) {
-      let cardContainer = document.createElement("div");
-      cardContainer.className = "col";
-      let card = document.createElement("div");
-      card.className = "card w-100";
-      card.innerHTML = `<img
+  for (let i = 0; i < events.length; i++) {
+    let cardContainer = document.createElement("div");
+    cardContainer.className = "col";
+    let card = document.createElement("div");
+    card.className = "card w-100";
+    card.innerHTML = `<img
     src=${events[i].image}
     class="image-card card-img-top"
     alt="imagen de concierto de musica"
@@ -44,72 +51,8 @@ const cardsHome = (array) => {
       <a href="#" class="btn btn-success">Details</a>
     </div>`;
 
-      containerGrid.appendChild(cardContainer);
-      cardContainer.appendChild(card);
-    }
-    // -------------------------- Cards Past Events --------------------------
-  } else if (document.URL.includes("pastEvents.html")) {
-    for (let i = 0; i < events.length; i++) {
-      if (currentDate > events[i].date) {
-        let cardContainer = document.createElement("div");
-        cardContainer.className = "col";
-        let card = document.createElement("div");
-        card.className = "card w-100";
-        card.innerHTML = `<img
-        src=${events[i].image}
-        class="image-card card-img-top"
-        alt="imagen de concierto de musica"
-        />
-        <div class="card-body">
-        <h5 class="card-title text-center">${events[i].name}</h5>
-        <div class="d-flex justify-content-between align-items-center my-2">
-        <span class="badge rounded-pill text-bg-danger p-2">${events[i].category}</span>
-        <span class="badge rounded-pill text-bg-secondary p-2">${events[i].date}</span>
-        </div>
-        <p class="card-text text-center">
-        ${events[i].description}
-        </p>
-        <div class="d-flex justify-content-between align-items-center">
-        <span class="text-dark fw-bold">Price: ${events[i].price}</span>
-        <a href="#" class="btn btn-success">Details</a>
-        </div>`;
-
-        containerGrid.appendChild(cardContainer);
-        cardContainer.appendChild(card);
-      }
-    }
-
-    // ---------------------- Cards Upcoming Events ----------------------------
-  } else if (document.URL.includes("upcomingEvents.html")) {
-    for (let i = 0; i < events.length; i++) {
-      if (currentDate <= events[i].date) {
-        let cardContainer = document.createElement("div");
-        cardContainer.className = "col";
-        let card = document.createElement("div");
-        card.className = "card w-100";
-        card.innerHTML = `<img
-        src=${events[i].image}
-        class="image-card card-img-top"
-        alt="imagen de concierto de musica"
-        />
-        <div class="card-body">
-        <h5 class="card-title text-center">${events[i].name}</h5>
-        <div class="d-flex justify-content-between align-items-center my-2">
-        <span class="badge rounded-pill text-bg-danger p-2">${events[i].category}</span>
-        <span class="badge rounded-pill text-bg-secondary p-2">${events[i].date}</span>
-        </div>
-        <p class="card-text text-center">
-        ${events[i].description}
-        </p>
-        <div class="d-flex justify-content-between align-items-center">
-        <span class="text-dark fw-bold">Price: ${events[i].price}</span>
-        <a href="#" class="btn btn-success">Details</a>
-        </div>`;
-
-        containerGrid.appendChild(cardContainer);
-        cardContainer.appendChild(card);
-      }
-    }
+    containerGridHome.appendChild(cardContainer);
+    cardContainer.appendChild(card);
   }
 };
 
